99c21bbdbaa8c558d9f81fb08a5d3a85f1566492
aouledissa <aouledissayousri@gmail.com>
2022-11-25 20:02:04 +0100

    final

--- src/controllers/GatewayController/GatewayController.ts
+++ src/controllers/GatewayController/GatewayController.ts
@@ -20,6 +20,8 @@ export abstract class GatewayController {
 
     public static async signUp(payload: any){
 
+        console.log(HOSTS.userManagement)
+
         try{
 
             let signUpPayload = SignUpPayload.createSignUpPayload(payload)
